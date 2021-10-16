ab387061f3ca73b064f299ab3bfd12e335467b61
Oh Martin <oh.martin@vdm.io>
2021-10-16 09:48:25 +0200

    Comment out: JHtml::_('behavior.tabstate');

--- site/demo.php
+++ site/demo.php
@@ -21,7 +21,7 @@
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
-JHtml::_('behavior.tabstate');
+//JHtml::_('behavior.tabstate');
 
 // Set the component css/js
 $document = JFactory::getDocument();
